1440d9b415cf8148d0bde269c4cf625499e9cb5d
Kevin Mantei <kevin.mantei@arvato.com>
2026-02-25 10:51:36 +0100

    update config

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,48 @@
+return {
+  "saghen/blink.cmp",
+  config = function()
+    require("blink.cmp").setup(
+      {
+        fuzzy = { implementation = 'prefer_rust' },
+        signature = { enabled = true },
+        keymap = {
+            preset = "default",
+            ["<C-space>"] = {},
+            ["<C-p>"] = {},
+            ["<Tab>"] = { "select_and_accept" },
+            ["<S-Tab>"] = {},
+            ["<C-y>"] = { "show", "show_documentation", "hide_documentation" },
+            ["<C-n>"] = { "select_and_accept" },
+            ["<C-k>"] = { "select_prev", "fallback" },
+            ["<C-j>"] = { "select_next", "fallback" },
+            ["<C-b>"] = { "scroll_documentation_down", "fallback" },
+            ["<C-f>"] = { "scroll_documentation_up", "fallback" },
+            ["<C-l>"] = { "snippet_forward", "fallback" },
+            ["<C-h>"] = { "snippet_backward", "fallback" },
+            -- ["<C-e>"] = { "hide" },
+        },
+
+        appearance = {
+            use_nvim_cmp_as_default = true,
+            nerd_font_variant = "normal",
+        },
+
+        completion = {
+            documentation = {
+                auto_show = true,
+                auto_show_delay_ms = 200,
+            }
+        },
+
+        cmdline = {
+            keymap = {
+                preset = 'inherit',
+                ['<CR>'] = { 'accept_and_enter', 'fallback' },
+            },
+        },
+
+        sources = { default = { "lsp" } }
+      }
+    )
+  end
+}
